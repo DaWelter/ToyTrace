7d8524f5abcb44b28e48184bcf0242559fc0e2dd
Michael Welter <mw.pub@welter-4d.de>
2017-10-02 20:37:35 +0200

    Multithreading! Terrible ad-hoc hacking and busy waiting but it seems to work fine.

--- naivetrace.cxx
+++ naivetrace.cxx
@@ -2,6 +2,47 @@
 #include "scene.hxx"
 #include "renderingalgorithms.hxx"
 #include <chrono>
+#include <atomic>
+#include <thread>
+
+static constexpr int COMM_WAITING = 0;
+static constexpr int COMM_HALT = 1;
+static constexpr int COMM_GO   = 2;
+
+
+void ThreadWorker(std::atomic_int &shared_pixel_index, int end_index, SpectralImageBuffer &buffer, const Scene &scene, std::atomic_int &shared_comms)
+{
+  Raytracing algo(scene);
+  while (true)
+  {
+    int comms = shared_comms.load();
+    if (comms == COMM_HALT)
+    {
+      shared_comms.store(COMM_WAITING);
+    }
+    else if (comms == COMM_WAITING)
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+    else if (comms == COMM_GO)
+    {
+      const int pixel_stride = 64 / sizeof(Spectral);
+      int pixel_index = shared_pixel_index.fetch_add(pixel_stride);
+      if (pixel_index >= end_index)
+        break;
+      int current_end_index = std::min(pixel_index + pixel_stride, end_index);
+      for (; pixel_index < current_end_index; ++pixel_index)
+      {
+        const int nsmpl = 64;
+        for(int i=0;i<nsmpl;i++)
+        {
+          Spectral smpl = algo.MakePrettyPixel(pixel_index);
+          buffer.Insert(pixel_index, smpl);
+        }
+      }
+    }
+  }
+}
 
 
 int main(int argc, char *argv[])
@@ -22,10 +63,10 @@ int main(int argc, char *argv[])
   
   int xres = scene.GetCamera().xres;
   int yres = scene.GetCamera().yres;
+  int num_pixels = xres*yres;
   
   Image bm(xres, yres);
   ImageDisplay display;
-  auto time_of_last_display = std::chrono::steady_clock::now();
 
   //Bdpt algo(scene);
   Raytracing algo(scene);
@@ -33,36 +74,60 @@ int main(int argc, char *argv[])
 
   SpectralImageBuffer buffer{xres, yres};
   
-  const int nsmpl = 4;
+  const int num_threads = 4;
+  
+  auto start_time = std::chrono::steady_clock::now();
   
   int image_conversion_y_start = 0;
+  
+  std::atomic_int shared_pixel_index(0);
+  
+  std::vector<std::atomic_int> thread_comm(num_threads);
+  std::vector<std::thread> threads;
+  
+  for (int i=0; i<num_threads; ++i)
+  {
+    thread_comm[i].store(COMM_GO);
+    threads.push_back(std::thread{
+      ThreadWorker, std::ref(shared_pixel_index), num_pixels, std::ref(buffer), std::cref(scene), std::ref(thread_comm[i])
+    });
+  }
   std::cout << std::endl;
   std::cout << "Rendering ..." << std::endl;
-  for (int y=0;y<yres;y++) 
+
+  while (shared_pixel_index.load() < num_pixels)
   {
-    for (int x=0;x<xres;x++) 
-    {    
-      int pixel_index = scene.GetCamera().PixelToUnit({x,y});
-      for(int i=0;i<nsmpl;i++)
-      {
-        Spectral smpl = algo.MakePrettyPixel(pixel_index);
-        buffer.Insert(pixel_index, smpl);
-      }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    
+    bool ok = false;
+    for (auto &comm : thread_comm)
+      comm.store(COMM_HALT);
+    for (auto &comm : thread_comm)
+    {
+      while (comm.load() != COMM_WAITING && shared_pixel_index.load() < num_pixels)
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
     
-    auto time = std::chrono::steady_clock::now();
-    if (time - time_of_last_display > std::chrono::seconds(1))
-    {
-      buffer.ToImage(bm, image_conversion_y_start, y);
-      image_conversion_y_start = y;
-      display.show(bm);
-      time_of_last_display = time;
-    }
+    int pixel_index = shared_pixel_index.load();
+    int y = pixel_index/xres - 1;
+    buffer.ToImage(bm, image_conversion_y_start, y);
+    image_conversion_y_start = y;
+
+    for (auto &comm : thread_comm)
+      comm.store(COMM_GO);    
+    
+    display.show(bm);
   }
+  
+  for (int i=0; i<threads.size(); ++i)
+    threads[i].join();
+
+  auto end_time = std::chrono::steady_clock::now();
+  std::cout << "Rendering time: " << std::chrono::duration<double>(end_time - start_time).count() << std::endl;
 
   buffer.ToImage(bm, image_conversion_y_start, yres);
   
   bm.write("raytrace.tga");
 
-  return 1;
+  return 0;
 }
