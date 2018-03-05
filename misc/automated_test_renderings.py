1d62fe66d194e6b71d4fe5b3e8955fa4f31e0bf8
Michael Welter <mw.pub@welter-4d.de>
2018-03-05 09:36:07 +0100

    More automated test renderings. Also using the unittest module now.

--- misc/automated_test_renderings.py
+++ misc/automated_test_renderings.py
@@ -5,25 +5,40 @@ import os, sys
 import subprocess
 from math import pi
 import numpy as np
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import tempfile
 import datetime
 import hashlib
 import textwrap
 from PIL import Image, ImageDraw, ImageFont
+import unittest
+import logging
 # References for PIL:
 # http://pillow.readthedocs.io/en/5.0.0/reference/ImageDraw.html
 # https://stackoverflow.com/questions/30227466/combine-several-images-horizontally-with-python
 
 
 os.chdir('../scenes')
-destination_dir =     os.path.join('..', 'misc', 'renders-'+datetime.datetime.now().strftime('%d-%m-%Y-%H-%M'))
+destination_dir =     os.path.join('..', 'misc', 'renders-'+datetime.datetime.now().strftime('%d-%m-%Y-%H'))
 if not os.path.isdir(destination_dir):
     os.mkdir(destination_dir)
 scenefile_dir = os.path.join(destination_dir, 'scenes')
 
 
-def toytrace(scene, output_file, opts):
+def merge(d1, d2):
+    return dict(d1.items() + d2.items())
+
+
+def logRendering(name, opts):
+    if "--algo" in opts:
+        mode = opts['--algo']
+    else:
+        mode = 'pt'
+    logging.info("Render: '%s' (%s)", name, mode)
+
+
+def toytrace(scene, output_file, opts, name = "UNKNOWN"):
+    logRendering(name, opts)
     use_tmpfile = not output_file
     if use_tmpfile:
         output_file = tempfile.mktemp(suffix='.png', prefix='/tmp/')
@@ -49,28 +64,45 @@ def toytrace(scene, output_file, opts):
         return Image.open(output_file)
 
 
-def dumpToSceneDir(scenes, filename_prefix):
-    if not os.path.isdir(scenefile_dir):
-        os.mkdir(scenefile_dir)
-    for k, scene in scenes.items():
-        filename = os.path.join(scenefile_dir, filename_prefix+k+'.nff')
-        if os.path.exists(filename):
-            with open(filename, 'r') as f:
-                target_checksum = hashlib.md5(f.read()).digest()
-            checksum = hashlib.md5(scene).digest()
-            if target_checksum != checksum:
-                raise RuntimeError("Attempt to overwrite %s which is different from what would be written!" % filename)
-        else:
-            with open(filename, 'w') as f:
-                f.write(scene)
-        scenes[k] = filename
+class SceneCollection(object):
+    def __init__(self, name_prefix):
+        self._name_prefix = name_prefix
+        self._scenes = {}
+
+    def __setitem__(self, key, value):
+        self._scenes[key] = value
+
+    def __getitem__(self, key):
+        return self._scenes[key]
+
+    def items(self):
+        return self._scenes.items()
+
+    def dumpToSceneDir(scenes):
+        filename_prefix = scenes._name_prefix
+        if not os.path.isdir(scenefile_dir):
+            os.mkdir(scenefile_dir)
+        for k, scene in scenes.items():
+            filename = os.path.join(scenefile_dir, filename_prefix + k + '.nff')
+            if os.path.exists(filename):
+                with open(filename, 'r') as f:
+                    target_checksum = hashlib.md5(f.read()).digest()
+                checksum = hashlib.md5(scene).digest()
+                if target_checksum != checksum:
+                    raise RuntimeError(
+                        "Attempt to overwrite %s which is different from what would be written!" % filename)
+            else:
+                with open(filename, 'w') as f:
+                    f.write(scene)
+            scenes[k] = filename
 
 
-class CornelBoxLightTypes(object):
+
+class CornelBoxLightTypesScenes(SceneCollection):
     def __init__(self):
+        SceneCollection.__init__(self, 'cornelbox_')
         self.buildScenePieces()
         self.buildScenes()
-        self.items = self.scenes.items
 
     def buildScenePieces(self):
         self.light_specs = OrderedDict()
@@ -155,41 +187,66 @@ class CornelBoxLightTypes(object):
         diffuse boxmat 1 1 1 0.4
         m box_in_cornelbox.dae
         """
-        self.scenes = {}
         for name, light_spec in self.light_specs.items():
-            self.scenes[name] = textwrap.dedent(area_light_test_template.format(**{'light_spec' : light_spec}))
-
-    def dumpToSceneDir(self):
-        dumpToSceneDir(self.scenes, 'cornelbox_')
+            self[name] = textwrap.dedent(area_light_test_template.format(**{'light_spec' : light_spec}))
 
 
+class CornelBoxLightTypes(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.W = 320
+        cls.H = 320
+        cls.common_opt = { '-w' : cls.W, '-h' : cls.H}
+        cls.scenes = CornelBoxLightTypesScenes()
 
-def runCornelBoxLightTypes():
-    W = 320
-    H = 320
-    imagegrid = []
-    scenes = CornelBoxLightTypes()
-    for name, scene in scenes.items():
-        image_row = []
-        for mode in 'both bsdf lights'.split():
-            img = toytrace(scene, '', {'--max-spp' : 16, '-w' : W, '-h' : H, '--pt-sample-mode' : mode})
-            image_row.append((name, mode, img))
-            #print name, mode,'->', img
-        imagegrid.append(image_row)
-    new_im = Image.new('RGB', (W*3, 10+H*len(imagegrid)))
-    d = ImageDraw.Draw(new_im)
-    for i, row in enumerate(imagegrid):
-        for j, (name, mode, img) in enumerate(row):
-            new_im.paste(img, (j*W, 10+i*H))
-            if i == 0:
-                d.text((1 + j*W, 1), mode)
-            if j == 0:
-                d.text((1, 11+i*H), name)
-    new_im.save(os.path.join(destination_dir, 'arealights.png'))
+    def runTest(self):
+        self.test_forwardtraced()
+        self.test_bdpt()
+
+    def test_forwardtraced(self):
+        W, H = self.W, self.H
+        imagegrid = []
+        for name, scene in self.scenes.items():
+            image_row = []
+            for mode in 'both bsdf lights'.split():
+                img = toytrace(
+                    scene, '',
+                    merge(self.common_opt, { '--pt-sample-mode' : mode, '--max-spp' : 16, }),
+                    name = name)
+                image_row.append((name, mode, img))
+            imagegrid.append(image_row)
+        new_im = Image.new('RGB', (W*3, 10+H*len(imagegrid)))
+        d = ImageDraw.Draw(new_im)
+        for i, row in enumerate(imagegrid):
+            for j, (name, mode, img) in enumerate(row):
+                new_im.paste(img, (j*W, 10+i*H))
+                if i == 0:
+                    d.text((1 + j*W, 1), mode)
+                if j == 0:
+                    d.text((1, 11+i*H), name)
+        new_im.save(os.path.join(destination_dir, 'arealights.png'))
 
 
-class AtmosphereScenes(object):
+    def test_bdpt(self):
+        W, H = self.W, self.H
+        images = []
+        for name, scene in self.scenes.items():
+            img = toytrace(
+                scene, '',
+                merge(self.common_opt, {'--algo': 'bdpt', '--max-spp' : 4 }),
+                name = name)
+            images.append((name, img))
+        new_im  = Image.new('RGB', (W, H*len(images)))
+        d = ImageDraw.Draw(new_im)
+        for i, (name, im) in enumerate(images):
+            new_im.paste(im, (0, i*H))
+            d.text((1, i*H), name)
+        new_im.save(os.path.join(destination_dir, 'arealights_bdpt.png'))
+
+
+class AtmosphereScenes(SceneCollection):
     def __init__(self):
+        SceneCollection.__init__(self, 'atmosphere_')
         scene_template = """
         {camera_spec}
 
@@ -197,7 +254,6 @@ class AtmosphereScenes(object):
 
         include atmosphere.nff
         """
-        self.scenes = {}
         light_specs = {
             'zenit' : 'lsun 0 0 -1 100',
             'late' : 'lsun -1 0 -0.02 100',
@@ -223,7 +279,7 @@ class AtmosphereScenes(object):
         }
         for k, v in light_specs.items():
             for l, w in cam_specs.items():
-                self.scenes[l+'_'+k] = textwrap.dedent(scene_template.format(**{'light_spec' : v, 'camera_spec' : w}))
+                self[l+'_'+k] = textwrap.dedent(scene_template.format(**{'light_spec' : v, 'camera_spec' : w}))
 
         spacescene = """
         v
@@ -249,41 +305,60 @@ class AtmosphereScenes(object):
 
         include atmosphere_textured.nff
         """
-        self.scenes['space'] = textwrap.dedent(spacescene)
-        self.scenes['backlit'] = textwrap.dedent(backlit)
-        self.items = self.scenes.items
-
-    def dumpToSceneDir(self):
-        dumpToSceneDir(self.scenes, 'atmosphere_')
+        self['space'] = textwrap.dedent(spacescene)
+        self['backlit'] = textwrap.dedent(backlit)
 
 
-def runAtmosphereScenes():
-    W = 320
-    H = 320
-    for name, scene in AtmosphereScenes().items():
-        toytrace(scene, 'atmosphere_'+name+'.png', {'--max-spp' : 16, '-w' : W, '-h' : H })
+class Atmosphere(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.W = 320
+        cls.H = 320
+        cls.common_opt = {'--max-spp': 16, '-w': cls.W, '-h': cls.H}
+        cls.scenes = AtmosphereScenes()
+
+    def test_forwardtraced(self):
+        for name, scene in self.scenes.items():
+            toytrace(
+                scene, 'atmosphere_'+name+'_bdpt.png',
+                merge(self.common_opt, {'--algo' : 'bdpt'}),
+                name = name)
 
 
-class ScenesFromFiles(object):
+class VariousScenes(SceneCollection):
     def __init__(self):
-        self.scenes = {
-            'veach_mis_scene' : 'veach_mis_scene.nff',
-            'veach_mis_scene_distant' : 'veach_mis_scene_distant.nff',
-            'material_test_scene' : 'material_test_scene.nff'
-        }
-        self.items = self.scenes.items
+        SceneCollection.__init__(self, '')
+        self['veach_mis_scene'] = 'veach_mis_scene.nff'
+        self['veach_mis_scene_distant'] = 'veach_mis_scene_distant.nff'
+        self['material_test_scene'] = 'material_test_scene.nff'
+        self['cornel_parallel_arealight'] = 'test_cornel_parallel_arealight.nff'
+        self['cornelbox_parabolic_reflector'] = 'test_cornelbox_parabolic_reflector.nff'
+        self['env_sphere_mirror'] = 'test_env_sphere_mirror.nff'
 
 
-def runScenesFromFiles():
-    W = 480
-    H = 320
-    for name, scene in ScenesFromFiles().items():
-        toytrace(scene, name+'.png', {'--max-spp' : 16, '-w' : W, '-h' : H })
+class Various(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.common_opt = defaultdict(lambda : {'-w': 480, '-h': 320})
+        cls.common_opt['cornel_parallel_arealight'] = {'-w': 320, '-h': 320}
+        cls.scenes = VariousScenes()
+
+    def runSingle(self, name):
+        toytrace(
+            self.scenes[name], name + '.png',
+            merge(self.common_opt[name], {'--max-spp': 16}),
+            name = name)
+        toytrace(
+            self.scenes[name], name + '_bdpt.png',
+            merge(self.common_opt[name], { '--algo' : 'bdpt', '--max-spp': 4}),
+            name = name)
+
+    def runTest(self):
+        for name, scene in self.scenes.items():
+            self.runSingle(name)
+
 
 
 if __name__ == '__main__':
-    runScenesFromFiles()
-    #CornelBoxLightTypes().dumpToSceneDir()
-    #runCornelBoxLightTypes()
-    #runAtmosphereScenes()
-    #AtmosphereScenes().dumpToSceneDir()
+    logging.getLogger().setLevel("INFO")
+    unittest.main()
