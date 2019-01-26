9ea232708c3a731b229120b30e96dee760ff21e4
Michael Welter <mw.pub@welter-4d.de>
2019-01-26 23:11:17 +0100

    More experiments with TBB

--- src/tests_tbb.cxx
+++ src/tests_tbb.cxx
@@ -3,8 +3,13 @@
 #include <tbb/parallel_for.h>
 #include <tbb/tbb_thread.h>
 #include <tbb/task_scheduler_init.h>
+#include <tbb/parallel_do.h>
+#include <tbb/task_group.h>
+#include <tbb/task.h>
 #include <thread>
 #include <iostream>
+#include <boost/optional/optional.hpp>
+#include <boost/none.hpp>
 
 /*  What's this? Sketch of the logic which could potentially be used in my path tracing algorithm.
  *  
@@ -92,15 +97,129 @@ private:
 };
 
 
-int main(int argc, char **argv)
+template<class Func, class Feeder>
+struct while_parallel_fed_
+{
+  Func f;
+  Feeder feeder;
+  tbb::task_group *tg;
+  
+  void run(int n_tasks)
+  {
+    for (int worker_num=0; worker_num<n_tasks; ++worker_num)
+    {
+      tg->run([=]{parallel_op();});
+    }
+    tg->wait();
+  }
+  
+  void parallel_op()
+  {
+    auto val = feeder();
+    if (!val)
+      return;
+    f(*val);
+    tg->run([=]{parallel_op();});
+  }
+};
+
+
+template<class Func, class Feeder>
+void while_parallel_fed(Func &&f, Feeder &&feeder, int n_tasks, tbb::task_group &tg)
+{
+  while_parallel_fed_<Func,Feeder>{f, feeder, &tg}.run(n_tasks);
+}
+
+
+// Same as above but slightly different implementation
+class Demo2
+{
+  tbb::task_group other_kind_of_tg;
+  tbb::mutex mutex;  // Lock the frame buffer. Well here it actually locks std::cout.
+  int count = 50;
+  tbb::atomic<int> subtask_num = 0; // Iterate over pixels
+public:
+  static constexpr int n_tasks = 4;
+  void Run()
+  {
+    while (!is_done())
+    {
+      run_primary_tasks();
+      maybe_run_secondary_tasks();
+    }
+  }
+  
+  void run_primary_tasks()
+  {
+    while_parallel_fed(
+      /* func = */[=](int j)
+      {
+        this->printl("Running primary subtask ", j, ", threadid ", std::this_thread::get_id());
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      }, 
+      /*feeder = */ [=]() -> boost::optional<int> 
+      {
+        int j = subtask_num.fetch_and_increment();
+        return (j>=count) ? boost::optional<int>{} : j;
+      }, 
+      n_tasks, other_kind_of_tg);
+  }
+  
+  void maybe_run_secondary_tasks()
+  {
+    tbb::parallel_for(0, n_tasks, 1, [this](int worker_num){
+      this->printl("Running secondary ", worker_num, ", threadid ", std::this_thread::get_id());
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    });
+  }
+  
+  void interrupt()
+  {
+    other_kind_of_tg.cancel();
+  }
+  
+  bool is_done()
+  {
+    return subtask_num.load() > count;
+  }
+
+private:
+  // Nice debug output.
+  template<typename... Xs>
+  void printl(Xs...xs)
+  {
+    tbb::mutex::scoped_lock lock(mutex);
+    printl_recurse(xs...);
+  }
+  
+  template<typename X, typename... Xs>
+  void printl_recurse(X x, Xs...xs)
+  {
+    std::cout << x;
+    if constexpr(sizeof...(xs) > 0)
+      printl_recurse(xs...);
+    else
+      std::cout << std::endl;
+  }
+};
+
+
+void DoDemo()
 {
   tbb::task_scheduler_init init(Demo::n_tasks); // Not strictly required. But this way I can set the number of threads.
-  Demo d;
+  Demo2 d;
   // The demo algorithm must be run in a thread, so I can cause interruptions from the main thread.
   tbb::tbb_thread th([&]{d.Run();}); 
   std::this_thread::sleep_for(std::chrono::milliseconds(200));
   d.interrupt();
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
   d.interrupt();
-  th.join();
+  th.join();  
+}
+
+
+int main(int argc, char **argv)
+{
+  DoDemo();
+  return 0;
 }
