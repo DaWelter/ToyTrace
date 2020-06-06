f48f59bd4ee42c0f963e74f5e1cc6e0c970ec51e
Michael Welter <mw.pub@welter-4d.de>
2020-06-06 14:30:51 +0200

    Fix Eigen plugins

--- src/eigen_plugins/eigen_array_plugin.hxx
+++ src/eigen_plugins/eigen_array_plugin.hxx
@@ -3,12 +3,14 @@
     EIGEN_STRONG_INLINE Array(eigen_zero_initialize_t)
     {
       Base::_check_template_params();
-      Base::setZero(Base::SizeAtCompileTime);
+      //Base::setZero(Base::SizeAtCompileTime);
+      Base::setZero(Base::RowsAtCompileTime, Base::ColsAtCompileTime);
     }
 
     EIGEN_DEVICE_FUNC
     EIGEN_STRONG_INLINE Array(eigen_ones_initialize_t)
     {
       Base::_check_template_params();
-      Base::setOnes(Base::SizeAtCompileTime);
+      //Base::setOnes(Base::SizeAtCompileTime);
+      Base::setOnes(Base::RowsAtCompileTime, Base::ColsAtCompileTime);
     }
--- src/eigen_plugins/eigen_matrix_plugin.hxx
+++ src/eigen_plugins/eigen_matrix_plugin.hxx
@@ -3,7 +3,8 @@
     EIGEN_STRONG_INLINE Matrix(eigen_zero_initialize_t)
     {
       Base::_check_template_params();
-      Base::setZero(Base::SizeAtCompileTime);
+      //Base::setZero(Base::SizeAtCompileTime);
+      Base::setZero(Base::RowsAtCompileTime, Base::ColsAtCompileTime);
     }
 
   
@@ -11,5 +12,6 @@
     EIGEN_STRONG_INLINE Matrix(eigen_ones_initialize_t)
     {
       Base::_check_template_params();
-      Base::setOnes(Base::SizeAtCompileTime);
+      //Base::setOnes(Base::SizeAtCompileTime);
+      Base::setOnes(Base::RowsAtCompileTime, Base::ColsAtCompileTime);
     }
